f1160031ff5e2a194f924545691f306c6a8477d0
Arjun Bhalla <ab2383@cornell.edu>
2020-07-12 02:48:19 -0400

    pages?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Helmet} from 'react-helmet';
 import About from './pages/About';
 import Headbar from './components/Headbar/Headbar';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -10,18 +11,29 @@ import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Helmet><title>Arjun Bhalla</title></Helmet>
     <div>
       <Headbar />
       <Sidebar />
       <Switch>
         <Route exact path="/" component={About} />
         <Route path="/projects" component={Projects} />
-        {/* <Route path="/travel" component={Travel} /> */}
+        <Route path="/travel" component={Travel} />
         <Route path="/wine" component={Wine} />
-        {/* <Route path="/books" component={Books} /> */}
+        <Route path="/music" component={Music} />
       </Switch>
     </div>
   </BrowserRouter>
   ,
   document.getElementById('root')
 );
+
+function Travel() 
+{
+  return <p>Under Construction</p>
+}
+
+function Music() 
+{
+  return <p>Under Construction</p>
+}
