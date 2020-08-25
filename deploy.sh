b35490ac5447e5a30c7cbfb44a7bec4ce6abfda6
Arjun Bhalla <ab2383@cornell.edu>
2020-08-24 22:45:45 -0400

    Added build and deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+npm run build
+mkdir ./build/public
+mv ./build/asset-manifest.json ./build/public
+mv ./build/favicon.ico ./build/public
+mv ./build/index.html ./build/public
+mv ./build/static ./build/public
+netlify deploy --prod
